c6f1482d16c21022a6b16d943d07c76cd6929371
priramdona <137527807+priramdona@users.noreply.github.com>
2025-03-11 00:56:13 +0700

    test

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -395,7 +395,7 @@
 
 
     <!-- Team Start -->
-    {{-- <div class="container-fluid py-5 mb-5 team">
+    <div class="container-fluid py-5 mb-5 team">
         <div class="container">
             <div class="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
                 <h5 class="text-primary">Our Team</h5>
@@ -406,22 +406,22 @@
                     <div class="team-content">
                         <div class="team-img-icon">
                             <div class="team-img rounded-circle">
-                                <img src="img/team-1.jpg" class="img-fluid w-100 rounded-circle" alt="">
+                                <img src="img/Munggi Priramdona (Blue).jpg" class="img-fluid w-100 rounded-circle" alt="">
                             </div>
                             <div class="team-name text-center py-3">
-                                <h4 class="">Full Name</h4>
-                                <p class="m-0">Designation</p>
+                                <h4 class="">Munggi Priramdona</h4>
+                                <p class="m-0">Founder, Developers and Initiator</p>
                             </div>
                             <div class="team-icon d-flex justify-content-center pb-4">
-                                <a class="btn btn-square btn-secondary text-white rounded-circle m-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-secondary text-white rounded-circle m-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-secondary text-white rounded-circle m-1" href=""><i class="fab fa-instagram"></i></a>
-                                <a class="btn btn-square btn-secondary text-white rounded-circle m-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                                <a class="btn btn-square btn-secondary text-white rounded-circle m-1" href="https://web.facebook.com/munggi.priramdhona/"><i class="fab fa-facebook-f"></i></a>
+                                {{-- <a class="btn btn-square btn-secondary text-white rounded-circle m-1" href=""><i class="fab fa-twitter"></i></a> --}}
+                                <a class="btn btn-square btn-secondary text-white rounded-circle m-1" href="https://www.instagram.com/priramdhona/"><i class="fab fa-instagram"></i></a>
+                                <a class="btn btn-square btn-secondary text-white rounded-circle m-1" href="https://www.linkedin.com/in/munggi-priramdona-7aa02913b/"><i class="fab fa-linkedin-in"></i></a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="rounded team-item">
+                {{-- <div class="rounded team-item">
                     <div class="team-content">
                         <div class="team-img-icon">
                             <div class="team-img rounded-circle">
@@ -477,10 +477,10 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
-    </div> --}}
+    </div>
     <!-- Team End -->
 
     <!-- Testimonial Start -->
